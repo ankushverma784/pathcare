7c60cbc681f76020d2a5fd89d483f8547fa17512
ankush verma <you@example.com>
2020-03-29 23:37:44 +0530

    initial commit

---
+++ pathcare/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pathcare.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult index2()
+        {
+            return View();
+        }
+        public ActionResult service()
+        {
+            return View();
+        }
+        public ActionResult blog()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
